fee19c41984eb737fb9cf1d59126a2d0d78210eb
MacGiv <55637723+MacGiv@users.noreply.github.com>
2024-11-16 12:57:01 -0300

    Update GameManager

--- FlappyBird/src/GamePlay/Manager/gameManager.cpp
+++ FlappyBird/src/GamePlay/Manager/gameManager.cpp
@@ -1,10 +1,13 @@
 #include "gameManager.h"
 
-void GameManager::ResetGame(Player::Player& player, Texture2D& playerSheet, std::list<Pipe::PipeSet>& pipeSets, float& spawnTimer, bool& pause)
+void GameManager::ResetGame(Player::Player& p1, Player::Player& p2, Texture2D& playerOneSheet, Texture2D& playerTwoSheet, std::list<Pipe::PipeSet>& pipeSets, float& spawnTimer, bool& pause)
 {
-	player = Player::Player();
-
-	player.frameRec = { 0.0f, 0.0f, (float)playerSheet.width / 3, (float)playerSheet.height };
+	p1 = Player::Player();
+	p2 = Player::Player();
+	p1.id = 1;
+	p2.id = 2;
+	p1.frameRec = { 0.0f, 0.0f, (float)playerOneSheet.width / 3, (float)playerOneSheet.height };
+	p2.frameRec = { 0.0f, 0.0f, (float)playerOneSheet.width / 3, (float)playerTwoSheet.height };
 
 	pipeSets.clear();
 
@@ -13,22 +16,22 @@ void GameManager::ResetGame(Player::Player& player, Texture2D& playerSheet, std:
 	pause = false;
 }
 
-void GameManager::ShouldResetGame(Menus& gameState, Player::Player& player, Texture2D& playerSheet, std::list<Pipe::PipeSet>& pipeSets, float& spawnTimer, bool& pause)
+void GameManager::ShouldResetGame(Menu::Menus& gameState, Player::Player& p1, Player::Player& p2, Texture2D& p1Sheet, Texture2D& p2Sheet, std::list<Pipe::PipeSet>& pipeSets, float& spawnTimer, bool& pause)
 {
 	switch (gameState)
 	{
-	case Menus::MainMenu:
+	case Menu::Menus::MainMenu:
 
-		GameManager::ResetGame(player, playerSheet, pipeSets, spawnTimer, pause);
+		GameManager::ResetGame(p1, p2, p1Sheet, p2Sheet, pipeSets, spawnTimer, pause);
 		break;
 
-	case Menus::Replay:
+	case Menu::Menus::Replay:
 
-		GameManager::ResetGame(player, playerSheet, pipeSets, spawnTimer, pause);
-		gameState = Menus::Playing;
+		GameManager::ResetGame(p1, p2, p1Sheet, p2Sheet, pipeSets, spawnTimer, pause);
+		gameState = Menu::Menus::Playing;
 		break;
 
-	case Menus::Exit:
+	case Menu::Menus::Exit:
 		break;
 	default:
 		break;
--- FlappyBird/src/GamePlay/Manager/gameManager.h
+++ FlappyBird/src/GamePlay/Manager/gameManager.h
@@ -8,6 +8,6 @@
 
 namespace GameManager
 {
-	void ResetGame(Player::Player& player, Texture2D& playerSheet, std::list<Pipe::PipeSet>& pipeSets, float& spawnTimer, bool& pause);
-	void ShouldResetGame(Menus& gameState, Player::Player& player, Texture2D& playerSheet,std::list<Pipe::PipeSet>& pipeSets, float& spawnTimer, bool& pause);
+	void ResetGame(Player::Player& p1, Player::Player& p2, Texture2D& playerOneSheet, Texture2D& playerTwoSheet, std::list<Pipe::PipeSet>& pipeSets, float& spawnTimer, bool& pause);
+	void ShouldResetGame(Menu::Menus& gameState, Player::Player& p1, Player::Player& p2, Texture2D& p1Sheet, Texture2D& p2Sheet,std::list<Pipe::PipeSet>& pipeSets, float& spawnTimer, bool& pause);
 }
