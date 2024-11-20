ce159850674c1ea3bd7982b328e4a6c0a25d6814
MacGiv <tomasluchelli.17@gmail.com>
2024-11-20 01:58:44 -0300

    Update Scene.h

--- FlappyBird/src/Scenes/scene.h
+++ FlappyBird/src/Scenes/scene.h
@@ -10,11 +10,12 @@
 
 namespace Drawers
 {
-	void DrawGamePlay(Player::Player player, Player::Player playerTwo, std::list<Pipe::PipeSet>& pipeSets, Sprites::Sprites& sprites, Sprites::SpriteMovement spriteMovement, bool pause);
+	void DrawGamePlay(Menu::Menus gameState, Player::Player player, Player::Player playerTwo, std::list<Pipe::PipeSet>& pipeSets, Sprites::Sprites& sprites, Sprites::SpriteMovement spriteMovement, bool pause);
 	void DrawCredits(Menu::Menus& gameState, Font font);
 	void DrawMainMenu(Menu::Menus& gameState, Font font, Texture2D gamesTitle);
 	void DrawGameRules(Menu::Menus& gameState, Font customFont);
 	void DrawGameOver(Menu::Menus& gameState, Font font);
 	void DrawPauseMenu(Menu::Menus& gameState, Font font, bool& pause);
 	void DrawConfirmExit(Menu::Menus& gameState, Font font, Menu::Menus previusMenu);
+	void DrawHowToPlay(Menu::Menus& gameState, Font font);
 }
